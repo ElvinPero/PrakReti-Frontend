087b0b9f9e369d26cd6f784487bcf9d7ade22907
ELVIN pero <invincible142002@gmail.com>
2023-09-01 13:30:05 +0530

    marketplace added

---
+++ src/components/CardDetail.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const CardDetail = () => {
+  return (
+    <>
+    <div>CardDetail</div>
+    <div>CardDetail</div>
+    <div>CardDetail</div>
+    <div>CardDetail</div>
+    <div>CardDetail</div>
+    <div>CardDetail</div>
+    <div>CardDetail</div>
+    <div>CardDetail</div>
+    <div>CardDetail</div>
+    <div>CardDetail</div>
+    <div>CardDetail</div>
+    <div>CardDetail</div>
+   </>
+    )
+}
+
+export default CardDetail
